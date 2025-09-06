c8d3f42e9104b1edaff9a9f3cdeaa96c4cf4a24e
yanghua <yanghua1127@gmail.com>
2025-09-06 19:43:59 +0800

    refactor code

--- rust/lance-encoding/src/encodings/logical/primitive.rs
+++ rust/lance-encoding/src/encodings/logical/primitive.rs
@@ -3055,7 +3055,9 @@ impl StructuralDecodeArrayTask for StructuralCompositeDecodeArrayTask {
         let array = arrow_select::concat::concat(&array_refs)?;
         let mut repdef = CompositeRepDefUnraveler::new(unravelers);
 
+        debug!("---------> The array's len before restoring validity is {:?}", array.len());
         let array = Self::restore_validity(array, &mut repdef);
+        debug!("---------> The array's len after restoring validity is {:?}", array.len());
 
         Ok(DecodedArray { array, repdef })
     }
